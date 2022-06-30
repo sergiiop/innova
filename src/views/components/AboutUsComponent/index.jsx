628b45909e069f64d7c8d688b1c7208a4f424c11
juanfug <54547859+juanfug@users.noreply.github.com>
2022-06-30 10:24:32 -0500

    information
    update info footer and contact info

--- src/views/components/AboutUsComponent/index.jsx
+++ src/views/components/AboutUsComponent/index.jsx
@@ -9,17 +9,7 @@ const AboutUsComponent = () => {
       <TitleComponent>INNOVA CIENCIA Y TECNOLOGIA</TitleComponent>
       <AboutUsImg src={AboutUs} />
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae at non
-        pretium vitae tristique egestas. Pharetra, mattis purus, sit nunc,
-        tellus vitae magna. Vitae adipiscing cursus amet, risus. Ut et aliquam
-        lectus ut arcu, ac ac. Consequat, maecenas consequat, amet, amet, dictum
-        id. Sollicitudin dictum libero turpis eget at. Tincidunt ut ac a euismod
-        velit dolor sit. Leo luctus amet pellentesque vitae dignissim proin
-        faucibus sed. Gravida posuere arcu integer ut cras neque felis, nec. Sed
-        eget arcu, in nulla felis vitae amet. Malesuada dignissim neque nullam
-        ut. Semper consequat morbi cursus ut. Cursus non, euismod integer id
-        urna sodales ornare ultrices. Imperdiet suscipit enim orci, sed praesent
-        elementum. Dolor urna, augue senectus proin nunc, volutpat suspendisse.
+      Esta empresa fué constituida como SOCIEDAD POR ACCIONES SIMPLIFICADA y se dedica a Investigaciones y desarrollo experimental en el campo de las ciencias sociales y las humanidades.
       </p>
     </AboutUsContainer>
   )
